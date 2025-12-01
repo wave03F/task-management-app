b647e23e76b6f2ec2f7db8fa20a9c0b786dd637e
Phollphat Wangsawat <honasas1101@gmail.com>
2025-12-01 16:22:42 +0700

    add new

--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,6 +1,5 @@
-// src/task/dto/update-task.dto.ts
 import { TaskStatus } from '../task.entity';
-import { IsOptional, IsIn, IsString, IsNotEmpty } from 'class-validator';
+import { IsOptional, IsEnum, IsString, IsNotEmpty } from 'class-validator';
 
 export class UpdateTaskDto {
   @IsOptional()
@@ -14,6 +13,6 @@ export class UpdateTaskDto {
   description?: string;
 
   @IsOptional()
-  @IsIn(Object.values(TaskStatus)) // ตรวจสอบสถานะว่าถูกต้องตาม enum
+  @IsEnum(TaskStatus) // ตรวจสอบสถานะว่าถูกต้องตาม enum
   status?: TaskStatus;
 }
